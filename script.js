b01e99d2dcbe6bd2791ca472122e6fd6162f141a
Bohdan <dreamwat4er@gmail.com>
2019-04-14 16:58:26 +0300

    третий комит

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//let answer = prompt("ты тут?","no");
-console.log("2" === 2);
+let money=prompt("Ваш бюджет на месяц?");
+let time=prompt("Введите дату в формате YYYY-MM-DD");
+
+let nesessary=prompt("Введите обязательную статью расходов в этом месяце");
+let howNesessaryCost=prompt("Во сколько обойдется?");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses :{nesessary:howNesessaryCost},
+    optionalExpenses:"",
+    income :[],
+    savings :false
+};
+alert(money/30);
+
