979cce5e3544219def735dfebb2b8504f368af55
Bohdan <dreamwat4er@gmail.com>
2019-04-15 17:40:49 +0300

    пятый комит

--- script.js
+++ script.js
@@ -9,18 +9,36 @@ let appData = {
     income :[],
     savings :false
 };
-for (let i = 0; i < 2; i++) {
+/*for (let i = 0; i < 2; i++) {
    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
        b = prompt("Во сколько обойдется?",'');
 
-    if ( (typoff(a) === 'string') && (typoff(a) !== null) && (typoff(b) !== null)
-    && a !='' && b !=='' && a.length < 50){
+    if ( (typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(b)) !== null
+    && a !='' && b !='' && a.length < 50){
         console.log("done");
     }
-    else{
+   
+}*/
+/*let i=0;
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = prompt("Во сколько обойдется?",'');
 
-    }
-}
+ if ( (typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(b)) !== null
+ && a !='' && b !='' && a.length < 50){
+     console.log("done");
+    i++;
+}}*/
+
+let i=0;
+do {let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+       b = prompt("Во сколько обойдется",'');
+    if ( (typeof(a)) === 'string' && (typeof(a)) !==null && (typeof(b)) !==null && a !='' && b != '' && a.length<50) i++; 
+        console.log('done');
+    } while (i<2);
+
+
+
 
 appData.moneyPerDay = appData.budget/30;
 
@@ -33,5 +51,5 @@ if (appData.moneyPerDay < 100) {
 }   else if (appData.moneyPerDay >2000) {
         console.log("высокий уровень достатка");
 }   else {
-        console.log("произошла ошибка")
-}
+        console.log("произошла ошибка");
+};
