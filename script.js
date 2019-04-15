fd0f80936a09c4539db322ab6d6ea4d2902a5ac5
Bohdan <dreamwat4er@gmail.com>
2019-04-15 17:10:52 +0300

    четвертый комит

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-let money=prompt("Ваш бюджет на месяц?");
-let time=prompt("Введите дату в формате YYYY-MM-DD");
-
-let nesessary=prompt("Введите обязательную статью расходов в этом месяце");
-let howNesessaryCost=prompt("Во сколько обойдется?");
+let money= +prompt("Ваш бюджет на месяц?"),
+    time=prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses :{nesessary:howNesessaryCost},
+    expenses :{},
     optionalExpenses:"",
     income :[],
     savings :false
 };
-alert(money/30);
+for (let i = 0; i < 2; i++) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+       b = prompt("Во сколько обойдется?",'');
 
+    if ( (typoff(a) === 'string') && (typoff(a) !== null) && (typoff(b) !== null)
+    && a !='' && b !=='' && a.length < 50){
+        console.log("done");
+    }
+    else{
+
+    }
+}
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ежедневный бюджет" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("минимальный уровень достатка");
+}   else if(appData.moneyPerDay >100 && appData.moneyPerDay <2000){ 
+        console.log("средний уровень достатка");
+}   else if (appData.moneyPerDay >2000) {
+        console.log("высокий уровень достатка");
+}   else {
+        console.log("произошла ошибка")
+}
